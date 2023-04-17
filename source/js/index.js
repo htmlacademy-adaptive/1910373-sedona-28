a4929d1d25bdd7c18b7e6e49074713b40f48aea0
Alexander Trofimov <Sasha1233515744@gmail.com>
2023-04-17 16:15:58 +0300

    добавил медиа выражения и модалку

--- source/js/index.js
+++ source/js/index.js
@@ -1,27 +1,7 @@
 var burgerBtn = document.querySelector('.main-header__toggle');
-var navList = document.querySelector('.site-nav');
-var logo = document.querySelector('.main-nav__logo');
+var mainHeader = document.querySelector('.main-header');
 
+// mainHeader.classList.remove('no-js');
 burgerBtn.addEventListener('click', function(){
-  navList.classList.toggle('site-nav--hide');
-  logo.classList.toggle('main-nav__logo--hide');
-});
-
-document.addEventListener('keydown', function(evt){
-  if (evt.keyCode === 27) {
-    navList.classList.add('site-nav--hide');
-    logo.classList.remove('main-nav__logo--hide');
-  }
-});
-
-navList.addEventListener('click', function(){
-  navList.classList.add('site-nav--hide');
-});
-
-document.addEventListener('click', function(evt){
-  var click = evt.composedPath().includes(burgerBtn);
-  if(!click) {
-    navList.classList.add('site-nav--hide');
-    logo.classList.remove('main-nav__logo--hide');
-  }
+  mainHeader.classList.toggle('is-open');
 });
